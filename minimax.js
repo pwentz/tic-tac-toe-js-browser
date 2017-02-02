4302a5cce77a26561c77c7099fae9ebc5caf2c10
pwentz <patrick.w.wentz@gmail.com>
2017-02-02 11:59:17 -0600

    add functionality to minimax to calculate whether given marker is in position to win horizontally or diagonally.

--- minimax.js
+++ minimax.js
@@ -4,18 +4,39 @@ module.exports = class Minimax {
   }
 
   score(board, selectedMarker, currentMarker) {
-    const { outcome } = this
-    const openings = outcome.openSpaces(board)
-    const markerLocations = outcome.getIndex(board, selectedMarker)
-    const hasDownwardDiagonal = (markerLocations.includes(0) && markerLocations.includes(8) && openings.includes(4))
-    const hasUpwardDiagonal = (markerLocations.includes(2) && markerLocations.includes(6) && openings.includes(4))
-
     if (selectedMarker === currentMarker) {
-      if (hasDownwardDiagonal || hasUpwardDiagonal) {
+      if (this.inPositionDiagonally(board, selectedMarker)) {
+        return 10
+      }
+
+      if (this.inPositionHorizontally(board, selectedMarker)) {
         return 10
       }
     }
 
     return -10
+  }
+
+  inPositionHorizontally(board, marker) {
+    let isInPosition;
+
+    for (let i = 0 ; i < board.length ; i++) {
+      const nextThree = board.slice(i, i + 3)
+      const rowMarkers = nextThree.filter(m => m === marker)
+
+      if ((rowMarkers.length === 2) && nextThree.includes(' ')) {
+        isInPosition = true
+      }
+    }
+
+    return isInPosition || false
+  }
+
+  inPositionDiagonally(board, selectedMarker) {
+    const openings = this.outcome.openSpaces(board)
+    const markerLocations = this.outcome.getIndex(board, selectedMarker)
+
+    return (markerLocations.includes(0) && markerLocations.includes(8)) ||
+    ((markerLocations.includes(2) && markerLocations.includes(6)) && openings.includes(4))
   }
 }
--- test/minimax.test.js
+++ test/minimax.test.js
@@ -7,30 +7,58 @@ describe('Minimax', () => {
     const minimax = new Minimax(Outcome)
 
     describe('#score', () => {
-      context('current marker & selected marker is in direct position to win diagonally', () => {
-        it('returns 10 if markers are sloping downward', () => {
+      context('end game scenario', () => {
+        context('current marker & selected marker is in direct position to win diagonally', () => {
+          it('returns 10 if markers are sloping downward', () => {
+            const board = ['X', 'O', 'O',
+                           ' ', ' ', 'O',
+                           ' ', 'O', 'X']
+
+            assert.equal(minimax.score(board, 'X', 'X'), 10)
+          })
+
+          it('returns 10 if markers are sloping upwards', () => {
+            const board = [' ', 'O', 'X',
+                           ' ', ' ', 'O',
+                           'X', 'O', 'X']
+
+            assert.equal(minimax.score(board, 'X', 'X'), 10)
+          })
+        })
+
+        context('current marker & selected marker is in direct position to win horizontally', () => {
+          it('returns 10 if markers are aligned on top row', () => {
+            const board = ['X', ' ', 'X',
+                           'O', ' ', 'O',
+                           ' ', 'O', ' ']
+
+            assert.equal(minimax.score(board, 'X', 'X'), 10)
+          })
+
+          it('returns 10 if markers are aligned on middle row', () => {
+            const board = ['X', ' ', 'X',
+                           'O', ' ', 'O',
+                           ' ', 'O', 'X']
+
+            assert.equal(minimax.score(board, 'O', 'O'), 10)
+          })
+
+          it('returns 10 if markers are aligned on bottom row', () => {
+            const board = ['X', ' ', 'X',
+                           'O', ' ', 'X',
+                           'O', 'O', ' ']
+
+            assert.equal(minimax.score(board, 'O', 'O'), 10)
+          })
+        })
+
+        it('returns -10 if marker is in direct position to lose', () => {
           const board = ['X', 'O', 'O',
                          ' ', ' ', 'O',
                          ' ', 'O', 'X']
 
-          assert.equal(minimax.score(board, 'X', 'X'), 10)
+          assert.equal(minimax.score(board, 'X', 'O'), -10)
         })
-
-        it('returns 10 if markers are sloping upwards', () => {
-          const board = [' ', 'O', 'X',
-                         ' ', ' ', 'O',
-                         'X', 'O', 'X']
-
-          assert.equal(minimax.score(board, 'X', 'X'), 10)
-        })
-      })
-
-      it('returns -10 if marker is in direct position to lose', () => {
-        const board = ['X', 'O', 'O',
-                       ' ', ' ', 'O',
-                       ' ', 'O', 'X']
-
-        assert.equal(minimax.score(board, 'X', 'O'), -10)
       })
     })
   })
