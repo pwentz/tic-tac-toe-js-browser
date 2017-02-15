69fffc1fbc9efadd4aad182b1a51c0e929c5b2b9
pwentz <patrick.w.wentz@gmail.com>
2017-02-15 14:13:03 -0600

    remove transpose, add isFull() method to board interface.

--- src/util.js
+++ src/util.js
@@ -1,20 +1,4 @@
-const R = require('ramda')
-
-const splitBoard = (board) => {
-  return [board.slice(0, 3),
-          board.slice(3, 6),
-          board.slice(6, 9)]
-}
-
-const flatten = (nestedBoard) => {
-  return nestedBoard.reduce((res, i) => [...res, ...i], [])
-}
-
 module.exports = {
-  transpose(board) {
-    return flatten(R.transpose(splitBoard(board)))
-  },
-
   cellToCoords(cellNumber) {
     switch(cellNumber) {
       case 0:
