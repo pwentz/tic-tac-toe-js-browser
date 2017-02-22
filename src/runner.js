0326315fbe5a860f79e0519911a7579fc0ad9b68
pwentz <patrick.w.wentz@gmail.com>
2017-02-22 17:01:22 -0600

    implement first stages of dual UI - marker selection and isGoingFirst complete.

--- src/runner.js
+++ src/runner.js
@@ -1,5 +1,13 @@
-module.exports = class Runner {
-  constructor({ ui }) {
-    this.ui = ui
-  }
+module.exports = (ui) => {
+  const userSettings = { }
+  ui.getMarkerSettings(marker => {
+    console.log('marker: ', marker)
+
+    ui.getOrderSettings(isUserGoingFirst => {
+      if (isUserGoingFirst.slice(0, 1) === 'y') {
+        console.log('USER IS GOING FIRST')
+      }
+    })
+  })
+
 }
---
+++ terminal-UI/main.js
@@ -0,0 +1,4 @@
+const terminalUI = require('./terminalUI')
+const play = require('../src/runner')
+
+play(new terminalUI())
