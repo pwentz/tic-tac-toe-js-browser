0f9a7b8103688a6213409611888dc13dcc499586
pwentz <patrick.w.wentz@gmail.com>
2017-02-23 13:45:41 -0600

    add bare bones functionality on playing w/ multiple ui's, TO FIX: browser memory leak, input validations

--- src/runner.js
+++ src/runner.js
@@ -3,7 +3,50 @@ const gameOptions = require('./setup')
 const { game, board, isGameOver, cpu } = gameOptions(9)
 
 module.exports = (ui) => {
-  const userSettings = { }
+
+  const startUserTurn = () => {
+    return new Promise((resolve, reject) => {
+      return ui.promptUserForTurn(resolve)
+    })
+  }
+
+  const playUserTurn = () => {
+    return startUserTurn()
+      .then((selection) => {
+        return new Promise((resolve) => {
+          board.addMarker(game.markerOne, selection)
+          ui.drawMarkerOne(game.markerOne, selection)
+
+          resolve()
+        })
+      })
+  }
+
+  const playCpuTurn = () => {
+    return new Promise((resolve, reject) => {
+      const move = cpu.getMove(game)
+      board.addMarker(cpu.marker, move)
+      ui.drawMarkerTwo(cpu.marker, move)
+
+      resolve()
+    })
+  }
+
+  const play = (playFirstPlayerTurn, playSecondPlayerTurn) => {
+    playFirstPlayerTurn()
+     .then(() => {
+        const outcome = isGameOver()
+
+        if (outcome) {
+          ui.onGameOver(outcome)
+          return
+        }
+        else {
+          play(playSecondPlayerTurn, playFirstPlayerTurn)
+        }
+     })
+  }
+
   ui.getMarkerSettings()
     .then(marker => {
       const cpuMarker = marker === 'X' ? 'O' : 'X'
@@ -17,56 +60,58 @@ module.exports = (ui) => {
       ui.renderBoard()
 
       if (isUserGoingFirst.slice(0, 1) === 'y') {
-        return ui.promptUserForTurn()
-          .then((selection) => {
-            board.addMarker(game.markerOne, selection)
-            ui.drawMarkerOne(game.markerOne, selection)
+        play(playUserTurn, playCpuTurn)
+        // return ui.promptUserForTurn()
+        //   .then((selection) => {
+        //     board.addMarker(game.markerOne, selection)
+        //     ui.drawMarkerOne(game.markerOne, selection)
 
-            const userOutcome = isGameOver()
+        //     const userOutcome = isGameOver()
 
-            if (userOutcome) {
-              ui.onGameOver(userOutcome)
-              return
-            }
+        //     if (userOutcome) {
+        //       ui.onGameOver(userOutcome)
+        //       return
+        //     }
 
-            const move = cpu.getMove(game)
-            board.addMarker(cpu.marker, move)
+        //     const move = cpu.getMove(game)
+        //     board.addMarker(cpu.marker, move)
 
-            ui.drawMarkerTwo(cpu.marker, move)
-            const cpuOutcome = isGameOver()
+        //     ui.drawMarkerTwo(cpu.marker, move)
+        //     const cpuOutcome = isGameOver()
 
-            if (cpuOutcome) {
-              ui.onGameOver(cpuOutcome)
-              return
-            }
-          })
+        //     if (cpuOutcome) {
+        //       ui.onGameOver(cpuOutcome)
+        //       return
+        //     }
+        //   })
       }
       else {
-        const move = cpu.getMove(game)
-        board.addMarker(cpu.marker, move)
-        // check for game over
+        play(playCpuTurn, playUserTurn)
+        // const move = cpu.getMove(game)
+        // board.addMarker(cpu.marker, move)
+        // // check for game over
 
-        ui.drawMarkerTwo(cpu.marker, move)
+        // ui.drawMarkerTwo(cpu.marker, move)
 
-        const cpuOutcome = isGameOver()
+        // const cpuOutcome = isGameOver()
 
-        if (cpuOutcome) {
-          ui.onGameOver(cpuOutcome)
-          return
-        }
+        // if (cpuOutcome) {
+        //   ui.onGameOver(cpuOutcome)
+        //   return
+        // }
 
-        return ui.promptUserForTurn()
-          .then((selection) => {
-            board.addMarker(game.markerOne, selection)
-            ui.drawMarkerOne(game.markerOne, selection)
+        // return ui.promptUserForTurn()
+        //   .then((selection) => {
+        //     board.addMarker(game.markerOne, selection)
+        //     ui.drawMarkerOne(game.markerOne, selection)
 
-            const userOutcome = isGameOver()
+        //     const userOutcome = isGameOver()
 
-            if (userOutcome) {
-              ui.onGameOver(userOutcome)
-              return
-            }
-          })
+        //     if (userOutcome) {
+        //       ui.onGameOver(userOutcome)
+        //       return
+        //     }
+        //   })
       }
     })
     .catch(error => {
