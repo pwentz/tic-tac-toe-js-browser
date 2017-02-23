71006cda3791757d1b1203f5aecd6996428be6ae
pwentz <patrick.w.wentz@gmail.com>
2017-02-23 17:03:25 -0600

    add board validations.

--- terminalUI/inputValidations.js
+++ terminalUI/inputValidations.js
@@ -5,5 +5,11 @@ module.exports = {
       const firstLetter = formattedInput.slice(0, 1)
       return ((firstLetter === 'y') || (firstLetter === 'n'))
     }
+  ],
+
+  GAMEPLAY_VALIDATIONS: [
+    (input) => !isNaN(input),
+    (input) => parseInt(input) < 8,
+    (input) => parseInt(input) >= 0
   ]
 }
--- terminalUI/main.js
+++ terminalUI/main.js
@@ -3,5 +3,6 @@ const inputValidations = require('./inputValidations')
 const play = require('../src/runner')
 
 const orderValidations = inputValidations['ORDER_VALIDATIONS']
+//const gameplayValidations = inputValidations['GAMEPLAY_VALIDATIONS']
 
 play(new terminalUI({ orderValidations }))
